80aaba13fd899be4613c6bc597fc5342a6b1d1ff
Chandan <chandansahoo02468@gmail.com>
2024-07-14 22:45:50 +0530

    add day 1

---
+++ day1.js
@@ -0,0 +1,22 @@
+var num1=1;
+console.log(num1);
+let s1="Chandan";
+console.log(s1);
+const flag1=true;
+console.log(flag1);
+let num2=2;
+let s2="Sahoo";
+let flag2=true;
+const arr1=[1,2,3,4];
+const obj1={
+    name : "Chandan"
+}
+console.log(typeof num2)
+console.log(typeof s2)
+console.log(typeof flag2)
+console.log(typeof arr1)
+console.log(typeof obj1)
+let num3=5;
+num3=4;
+const num4=6;
+num4=5;
