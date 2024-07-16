c6290635504491fe4db152ef7d7251ca0bd1bf37
Chandan <chandansahoo02468@gmail.com>
2024-07-16 12:24:59 +0530

    add day 2

---
+++ day2.js
@@ -0,0 +1,24 @@
+let num1=5
+let num2=4;
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+console.log(num1+=1)
+console.log(num2-=1)
+
+let num3=5;
+let num4=4;
+console.log(num1>num2)
+console.log(num1<num2)
+console.log(num1>=num3)
+console.log(num2<=num4)
+console.log(num2==num4)
+console.log(num2===num3)
+
+console.log(num1==num3 &&  num2===num4)
+console.log(num1==num3 ||  num3===num4)
+console.log(!(num3===num4))
+
+num3>0?console.log("positive"):console.log("negative")
